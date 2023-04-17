5f2c82853617326d1d70ee1758bd51e529ffd0ef
hhimanshu <dev.atwork@yahoo.com>
2023-04-17 00:10:13 -0700

    fect(): ad support for Google pay (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,72 @@ import React from 'react';
 import './App.css';
 import {Button, Typography} from "@mui/material";
 
+const allowedCardNetworks = ["AMEX", "DISCOVER", "INTERAC", "JCB", "MASTERCARD", "VISA"];
+const allowedCardAuthMethods = ["PAN_ONLY", "CRYPTOGRAM_3DS"];
 
-const buildSupportedPaymentMethodData = () => {
-    // Example supported payment methods:
-     //return [{supportedMethods: "https://pay.google.com"}];
-    return [{supportedMethods: "https://bobbucks.dev/pay"}];
+const getGooglePaymentsConfiguration = () => {
+    return {
+        environment: 'TEST',
+        apiVersion: 2,
+        apiVersionMinor: 0,
+        merchantInfo: {
+            // A merchant ID is available after approval by Google.
+            // 'merchantId':'12345678901234567890',
+            merchantName: 'Example Merchant'
+        },
+        allowedPaymentMethods: [{
+            type: 'CARD',
+            parameters: {
+                allowedAuthMethods: allowedCardAuthMethods,
+                allowedCardNetworks: allowedCardNetworks//.map(network => network.toLowerCase())
+            },
+            tokenizationSpecification: {
+                type: 'PAYMENT_GATEWAY',
+                // Check with your payment gateway on the parameters to pass.
+                // @see {@link https://developers.google.com/pay/api/web/reference/request-objects#gateway}
+                parameters: {
+                    'gateway': 'example',
+                    'gatewayMerchantId': 'exampleGatewayMerchantId'
+                }
+            }
+        }]
+    };
 }
 
-const buildShoppingCartDetails = () => {
-    // Hardcoded for demo purposes:
-    return {
-        id: "order-123",
-        displayItems: [
-            {
-                label: "Example item",
-                amount: {currency: "USD", value: "1.00"},
-            },
-        ],
-        total: {
-            label: "Total",
-            amount: {currency: "USD", value: "1.00"},
+const createPaymentRequest = () => {
+    // Add support for the Google Pay API.
+    const methodData = [
+        {
+            supportedMethods: 'https://google.com/pay',
+            data: getGooglePaymentsConfiguration()
         },
+        {
+            supportedMethods: 'basic-card',
+            data: getGooglePaymentsConfiguration()
+        }
+    ]
+
+    const details = {
+        total: {label: 'Test Purchase', amount: {currency: 'USD', value: '99.00'}}
     };
+
+    const options = {
+        requestPayerEmail: true,
+        requestPayerName: true
+    };
+
+    return new PaymentRequest(methodData, details);
 }
 
 const App = () => {
     const handlePaymentFlow = () => {
-        const request = new PaymentRequest(
-            buildSupportedPaymentMethodData(),
-            buildShoppingCartDetails()
-        );
+        const request = createPaymentRequest()
+
+        if (!request.canMakePayment()) {
+            alert("Browser does not support payment")
+            console.error("The browser does not support making payments, use other strategy")
+            return;
+        }
 
         request.show().then((paymentResponse) => {
             // Here we would process the payment. For this demo, simulate immediate success:
