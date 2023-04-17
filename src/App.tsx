db7ada8bd962cf0caae1fe87c03db78fe831a042
hhimanshu <dev.atwork@yahoo.com>
2023-04-16 23:43:27 -0700

    fect(): making payment button work for bobducks

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,59 @@ import './App.css';
 import {Button, Typography} from "@mui/material";
 
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <Button variant="contained">
-            <Typography fontFamily={"Source Sans Pro"}
-                        variant={"subtitle2"}
-                        fontWeight={"bold"}
-            >Pay $10</Typography>
-        </Button>
-      </div>
-    </div>
-  );
+const buildSupportedPaymentMethodData = () => {
+    // Example supported payment methods:
+     //return [{supportedMethods: "https://pay.google.com"}];
+    return [{supportedMethods: "https://bobbucks.dev/pay"}];
+}
+
+const buildShoppingCartDetails = () => {
+    // Hardcoded for demo purposes:
+    return {
+        id: "order-123",
+        displayItems: [
+            {
+                label: "Example item",
+                amount: {currency: "USD", value: "1.00"},
+            },
+        ],
+        total: {
+            label: "Total",
+            amount: {currency: "USD", value: "1.00"},
+        },
+    };
+}
+
+const App = () => {
+    const handlePaymentFlow = () => {
+        const request = new PaymentRequest(
+            buildSupportedPaymentMethodData(),
+            buildShoppingCartDetails()
+        );
+
+        request.show().then((paymentResponse) => {
+            // Here we would process the payment. For this demo, simulate immediate success:
+            paymentResponse.complete("success").then(() => {
+                // For demo purposes:
+                //introPanel.style.display = "none";
+                //successPanel.style.display = "block";
+            });
+            console.log(paymentResponse)
+        });
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <Button variant="contained" onClick={handlePaymentFlow}>
+                    <Typography fontFamily={"Source Sans Pro"}
+                                variant={"subtitle2"}
+                                fontWeight={"bold"}
+                    >Pay $10</Typography>
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
