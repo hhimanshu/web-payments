02cb7e0e4907546cf5b05fa8bd9b2cc8367dadd9
hhimanshu <dev.atwork@yahoo.com>
2023-04-16 22:58:30 -0700

    fect(): add basic dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+import "@fontsource/source-sans-pro"
+import "@fontsource/source-sans-pro/700.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
