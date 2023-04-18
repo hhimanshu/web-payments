04078d4c2322e8dcf52019ce9b2401581e8b8c10
hhimanshu <dev.atwork@yahoo.com>
2023-04-18 11:14:13 -0700

    feat(): use capacitor to generate ios and android bundle

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bonsaiilabs.payments',
+  appName: 'web-payments',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
